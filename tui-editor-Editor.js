c91b6518262cfaf90dc2ef60dd70904da2b6224d
Mike Konopelski <mikek@squareup.com>
2019-01-14 11:52:16 -0800

    do not escape quotes

--- tui-editor-Editor.js
+++ tui-editor-Editor.js
@@ -4312,7 +4312,7 @@ function isCodeFenceToken(token) {
  * @ignore
  */
 function escape(html, encode) {
-    return html.replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
+    return html;
 }
 /* eslint-enable */
 
